fbbc722604e2f568e9037f6e09ac6d715b6dc4d9
Jeff Kelling <jeff.kelling.1@gmail.com>
2021-10-20 14:39:19 -0500

    Refactor App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
-import Nav from './components/Nav';
 
 function App() {
 
   return (
     <div>
-      <main>
-        <Header></Header>
-      </main>
+      <Header></Header>
+      <main></main>
     </div>
   );
 }
