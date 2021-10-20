777ded4f1bee1e935a26bbf342142c084ee1a616
Jeff Kelling <jeff.kelling.1@gmail.com>
2021-10-20 15:18:55 -0500

    Create Footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <ul>
+        <li>
+          <a href="https://github.com/mews0">GitHub</a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/jeff-kelling">LinkedIn</a>
+        </li>
+        <li>
+          <a href="https://stackoverflow.com/users/15856623/mews0">Stack Overflow</a>
+        </li>
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
